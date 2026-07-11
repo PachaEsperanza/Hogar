1c49a481601c5ed3b2f532bca6454e90b3a2d548
LegadoInca <kjz4kz2db5@privaterelay.appleid.com>
2026-07-11 12:27:46 -0500

    ds

--- src/router/config.tsx
+++ src/router/config.tsx
@@ -1,16 +1,11 @@
 import type { RouteObject } from "react-router-dom";
 import NotFound from "../pages/NotFound";
 import Home from "../pages/home/page";
-import Empresas from "../pages/empresas/page";
 
 const routes: RouteObject[] = [
   {
     path: "/",
     element: <Home />,
-  },
-  {
-    path: "/empresas",
-    element: <Empresas />,
   },
   {
     path: "*",
