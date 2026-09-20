92054ded828f6e863f7cd9b3e69a325077cf82c8
LegadoInca <kjz4kz2db5@privaterelay.appleid.com>
2026-09-20 10:15:46 -0500

    c

--- src/pages/home/components/Hero.tsx
+++ src/pages/home/components/Hero.tsx
@@ -74,7 +74,7 @@ const Hero = () => {
             display: 'block',
             fontFamily: "'Josefin Sans', sans-serif",
             fontSize: 'clamp(0.9rem,2.2vw,1.8rem)',
-            fontWeight: 100,
+            fontWeight: 300,
             letterSpacing: '0.55em',
             color: '#D9A441',
             textAlign: 'center',
